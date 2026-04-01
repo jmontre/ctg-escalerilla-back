40a2bdc95ef4eebe9e545e375e6eae1742392093
javiermontre <javier.montre@evol.energy>
2026-04-01 15:38:57 -0300

    fix: ScheduleModule duplicado causaba doble ejecución del cron

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { ChallengesCronService } from './challenges-cron.service';
 import { CronController } from './cron.controller';
 import { ChallengesModule } from '../challenges/challenges.module';
 
 @Module({
   imports: [
-    ScheduleModule.forRoot(),
     ChallengesModule
   ],
   controllers: [CronController],
